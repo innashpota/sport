e99087b3f0105f16da5a8550e5e68018ff31b4de
Inna <shpota_inna@ukr.net>
2019-12-08 22:02:14 +0200

    Added GET methods

--- src/main/java/com/inna/shpota/sport/controller/CompetitionController.java
+++ src/main/java/com/inna/shpota/sport/controller/CompetitionController.java
@@ -1,7 +1,9 @@
 package com.inna.shpota.sport.controller;
 
 import com.inna.shpota.sport.data.MatchResult;
+import com.inna.shpota.sport.data.Result;
 import com.inna.shpota.sport.models.Competition;
+import com.inna.shpota.sport.models.Match;
 import com.inna.shpota.sport.models.Round;
 import com.inna.shpota.sport.service.CompetitionService;
 import org.springframework.util.Assert;
@@ -35,5 +37,20 @@ public class CompetitionController {
             @RequestBody MatchResult matchResult
     ) {
         service.setWinner(name, matchResult);
+    }
+
+    @GetMapping("/{name}/playedMatches")
+    public List<Match> playedMatches(@PathVariable("name") String name) {
+        return service.playedMatches(name);
+    }
+
+    @GetMapping("/{name}/notPlayedMatches")
+    public List<Match> notPlayedMatches(@PathVariable("name") String name) {
+        return service.notPlayedMatches(name);
+    }
+
+    @GetMapping("/{name}/results")
+    public List<Result> results(@PathVariable("name") String name) {
+        return service.results(name);
     }
 }
